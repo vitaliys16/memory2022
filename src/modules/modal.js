a195de69563c316bf1c26fecd35f5ea4eec2b998
Vitaliy Yurchenko <world-rom@yandex.ru>
2022-04-27 01:24:28 +0400

    Update: edit JS for  Modal Windows

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,11 +1,20 @@
 const modal = () => {
     const loginBtn = document.querySelector('.navbar-links__login');
+    const modal = document.querySelector('.login');
+    const modalWindow = document.querySelector('.login-modal');
+    const modalClose = document.querySelector('.login-modal__close');
 
     loginBtn.addEventListener("click", (btn) => {
         btn.preventDefault();
-        console.log('SUKA');
+        modal.style.display = 'block';
     });
-    console.log(loginBtn);
+
+    modal.addEventListener('click', (e) => { //при клике мимо модального окна само окно должно закрываться
+        if (!e.target.closest('.login-modal') || e.target.closest('.login-modal__close')) {
+            modal.style.display = 'none';  
+            modal.style = '';  
+        }
+    });
 };
 
 export default modal;
